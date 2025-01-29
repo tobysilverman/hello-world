f58f0f74e3df934d0bea171172f710e38eebe488
Toby Silverman <tobysilverman@Tobys-MacBook-Pro.local>
2025-01-28 22:08:20 -0800

    Added interactivity yo

---
+++ script.js
@@ -0,0 +1,7 @@
+// Find the button by its ID
+const myButton = document.getElementById("myButton");
+
+// Add a click event to the button
+myButton.addEventListener("click", function() {
+    alert("You clicked the button!");
+});
